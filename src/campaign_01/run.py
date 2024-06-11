e00a1a5548531ee9f4271796fed485c9d9fdde10
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2024-06-11 17:41:28 +0200

    Allow to specify custom node_sets, add legend

--- src/campaign_01/run.py
+++ src/campaign_01/run.py
@@ -11,12 +11,10 @@ from common.utils import L, run_analysis
 
 
 def _get_ids(
-    simulation: Simulation, node_set_name: Optional[str], cell_step: int = 1
+    simulation: Simulation, node_set: Optional[str | dict], cell_step: int = 1
 ) -> CircuitNodeIds:
-    if node_set_name:
-        node_set = simulation.node_sets[node_set_name]
-    else:
-        node_set = None
+    if isinstance(node_set, str):
+        node_set = simulation.node_sets[node_set]
     ids = simulation.circuit.nodes.ids(group=node_set)
     ids = ids[::cell_step] if cell_step != 1 else ids
     return ids
@@ -44,7 +42,7 @@ def _plot(index: int, path: str, conditions: dict, analysis_config: dict) -> Pat
     for node_set in node_sets:
         ids = _get_ids(
             simulation,
-            node_set_name=node_set,
+            node_set=node_set,
             cell_step=cell_step,
         )
         L.info("Node set: %s, ids: %s", node_set, len(ids))
@@ -58,8 +56,12 @@ def _plot(index: int, path: str, conditions: dict, analysis_config: dict) -> Pat
     if not ax:
         return None
     label = "  ".join(f"{k}={v}" for k, v in conditions.items())
-    ax.set_title(f"My plot {index}: {label}")
-    ax.figure.savefig(file_name)
+    ax.set_title(
+        f"{analysis_config['report_type']} {analysis_config['report_name']} {index}: {label}"
+    )
+    legend_labels = [str(i) for i in node_sets]
+    ax.legend(legend_labels, loc="upper center", bbox_to_anchor=(0.5, -0.1))
+    ax.figure.savefig(file_name, bbox_inches="tight")
     ax.figure.clear()
     ax.cla()
     return file_name
